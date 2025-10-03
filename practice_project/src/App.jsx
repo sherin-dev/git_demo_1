a84381d2abd0e98ae383a8644bd195dc4d98a8a0
sherin-dev <sherinshanavas123@gmail.com>
2025-10-03 12:30:55 +0530

    adding a button to branch sherin_1

--- practice_project/src/App.jsx
+++ practice_project/src/App.jsx
@@ -5,7 +5,9 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  function handleClick1(){
+    console.log("Hello Sherin");
+  }
   return (
     <>
       <div>
@@ -20,7 +22,8 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button>  
+        <button onClick={handleClick1}> Sherin</button>      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
